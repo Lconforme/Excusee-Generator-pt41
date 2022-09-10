1ddf52ab53f5105d0b64b19a6ac409fc7080a289
Lconforme <Lconforme06@yahoo.com>
2022-09-10 15:27:43 +0000

    done with project

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let generateExcuse = () => {
+  let pronoun = ["A", "The"];
+  let subject = ["teacher", "cat", "drummer", "mother", "personal trainer"];
+  let action = ["played my", "forked my", "wet my", "canned my"];
+  let possesion = ["book", "chair", "iphone", "water jug"];
+  let where = [
+    "at the pool",
+    "at the house",
+    "on the boat",
+    "on the carnival cruise"
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronounIndex = pronoun[Math.floor(Math.random()) * pronoun.length];
+  let subjectIndex = subject[Math.floor(Math.random()) * subject.length];
+  let actionIndex = action[Math.floor(Math.random()) * action.length];
+  let possesionIndex = possesion[Math.floor(Math.random()) * possesion.length];
+  let whereIndex = where[Math.floor(Math.random()) * where.length];
+
+  return (
+    pronounIndex +
+    " " +
+    subjectIndex +
+    " " +
+    actionIndex +
+    " " +
+    possesionIndex +
+    " " +
+    whereIndex
+  );
 };
+document.getElementById("the-excuse").innerHTML = generateExcuse();
